ba9b4cef96b041477625ec7d399e1052c03776b6
subsider <ccsanroman@gmail.com>
2019-01-09 13:03:42 +0100

    Apply subresource and add comment to a blog post using /api/blog_posts/{id}

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -53,6 +53,7 @@ class Comment implements AuthoredEntityInterface, PublishedDateEntityInterface
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\BlogPost", inversedBy="comments")
      * @ORM\JoinColumn(nullable=false)
+     * @Groups({"post"})
      */
     private $blogPost;
 
