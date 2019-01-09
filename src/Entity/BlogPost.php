778a332d7a20f03244b4ddcfed201c4c289cd204
subsider <ccsanroman@gmail.com>
2019-01-09 10:43:41 +0100

    User password encoder event subscriber

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -6,7 +6,6 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ApiResource(
@@ -21,13 +20,11 @@ class BlogPost
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
-     * @Groups({"read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"read"})
      */
     private $title;
 
