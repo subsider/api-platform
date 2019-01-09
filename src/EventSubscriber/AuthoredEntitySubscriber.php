eb43e7edbc054789904d991da237bc45418c123c
subsider <ccsanroman@gmail.com>
2019-01-09 11:52:12 +0100

    Add event subscriber to prepopulate author when storing a new post

---
+++ src/EventSubscriber/AuthoredEntitySubscriber.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\BlogPost;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class AuthoredEntitySubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    /**
+     * AuthoredEntitySubscriber constructor.
+     * @param TokenStorageInterface $tokenStorage
+     */
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+           KernelEvents::VIEW => ['getAuthenticatedUser', EventPriorities::PRE_WRITE],
+        ];
+    }
+
+    public function getAuthenticatedUser(GetResponseForControllerResultEvent $event)
+    {
+        $entity = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        $author = $this->tokenStorage->getToken()->getUser();
+
+        if (!$entity instanceof BlogPost || Request::METHOD_POST !== $method) {
+            return;
+        }
+
+        $entity->setAuthor($author);
+    }
+}
