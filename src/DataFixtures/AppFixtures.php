897a2b407e17338a18663a0667c9de4c63338709
subsider <ccsanroman@gmail.com>
2019-01-09 08:58:29 +0100

    Fixtures for blog post

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\BlogPost;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,9 +10,14 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A first blog post!');
+        $blogPost->setSlug('a-first-blog-post');
+        $blogPost->setPublished(new \DateTime('2018-01-09 12:00:00'));
+        $blogPost->setContent('Content of the blog post');
+        $blogPost->setAuthor('Piotr Jura');
 
+        $manager->persist($blogPost);
         $manager->flush();
     }
 }
